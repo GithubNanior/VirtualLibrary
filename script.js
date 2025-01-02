ce31be0fdef9c25c1eeb39ed190c8620e3fec492
GithubNanior <jakobspeybrouck@gmail.com>
2025-01-02 11:50:10 +0200

    Make example books added by script

--- script.js
+++ script.js
@@ -70,3 +70,60 @@ function removeBookElement(title)
     let book = document.querySelector(`.book[bookTitle="${title}"]`);
     bookContainer.removeChild(book);
 }
+
+addBook(
+    "A sea apart", 
+    `Anna was shocked when her twin sister, Lucia, who she thought
+    was always inseparable with, suddenly left her to study abroad.<br>
+    Follow Anna on a journey of discovery and growth, as she unravel
+    why her sister left without a word, and about herself...`
+);
+
+addBook(
+    "Adventure of a Jhonny", 
+    `Jhonny's back again! Now with another adventure with you 
+    guessed it- Rick and Mike! What will Jhonny's beer tainted 
+    doings bring us this time? What is Goddachup boy plotting?<br>
+    Find out in today's dose of Jhonny comedy!`
+);
+
+addBook(
+    "Anywhere but here",
+    `"Please... Anywhere but here..." the little boy said to
+    the fairy, he's bored of his home, his life, his bedroom, 
+    dark rings from many sleepless nights are clearly visible 
+    under his eyes.<br>
+    The fairy looked at him silently for a moment, sitting on 
+    the balustade... She then smiled understandingly, and held 
+    out her hand, the warm hands that guided him through a 
+    dream-like world...`
+);
+
+addBook(
+    "Morguean'fitness",
+    `If you aren't fit, you'll end up in the morgue sooner!
+    Any 5 year ol' can tell you that! So pick this book up,
+    the sooner the better, and learn and follow and keep on
+    trainin'!`
+);
+
+addBook(
+    "Life in ancient Egypt",
+    `History of the civilization of ancient Egypt, a lively description
+    of the occupation, food, trade, locations of interest, and beliefs 
+    of our ancestors along the Nile!`
+);
+
+addBook(
+    "Grimm's fairytales",
+    `A complete collection of the original Grim(m's) fairytales, 
+    with much violence, vengeance and justice!`
+);
+
+addBook(
+    "The jester who farts songs",
+    `There was once, a court jester, who farted songs from his behind.<br>
+    The jester had some real mad skills, and this story has some real
+    weird truths, open this book to find out who, and where this old
+    fart stunk the court!`
+);
