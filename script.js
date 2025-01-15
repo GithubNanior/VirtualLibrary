64f13e7b8aa98eba89720fe66b0c700e405ea398
GithubNanior <jakobspeybrouck@gmail.com>
2025-01-15 18:20:13 +0200

    Add Database, Interface and Bindings module

--- script.js
+++ script.js
@@ -1,105 +1,156 @@
-// Book data management
+const Database = (function(){
+    let books = [];
 
-let books = [];
-
-function Book(title, content)
-{
-    this.title = title;
-    this.content = content;
-}
-
-function addBook(title, content)
-{
-    title = title.trim();
-
-    if (findBook(title) != -1) 
+    function Book(title, content)
     {
-        return `Title "${title}" is already taken.`;
+        this.title = title;
+        this.content = content;
     }
-    else
+
+    function addBook(title, content)
     {
-        books.push(new Book(title, content));
+        title = title.trim();
 
-        addBookElement(title, content);
-    }
-}
-
-function removeBook(title)
-{
-    let index = findBook(title);
-    if (index == -1)
-    {
-        return `No book named "${title} found.`;
-    }
-    else
-    {
-        books.splice(index, 1);
-        removeBookElement(title);
-    }
-}
-
-function findBook(title)
-{
-    return books.findIndex((book)=>book.title == title);
-}
-
-// Book ui management
-
-let bookContainer = document.querySelector("main");
-
-function addBookElement(title, summary)
-{
-    let book = document.createElement("div");
-    book.classList.add("book");
-    book.setAttribute("bookTitle", title);
-
-    book.innerHTML = `
-    <div class="cover">
-        <h2></h2>
-    </div>
-    <div class="back">
-        <p></p>
-        <div class="book-actions">
-            <button><img src="images/view.svg" alt="View"></button>
-            <button><img src="images/delete.svg" alt="Remove"></button>
-        </div>
-    </div>
-    <div class="spine"></div>
-    `
-
-    book.querySelector(".cover > h2").textContent = title;
-
-    book.querySelector("button:has(img[alt='Remove'])").addEventListener("click", ()=>{
-        removeBook(title);
-    });
-
-    let back = book.querySelector(".back > p");
-    let lines = summary.split("\n");
-    let i = 0;
-    for (; i < lines.length; i++)
-    {
-        if (lines[i].trim() != '')
+        if (findBook(title) != -1) 
         {
-            break;
+            return `Title "${title}" is already taken.`;
+        }
+        else
+        {
+            books.push(new Book(title, content));
+
+            Interface.addBookElement(title, content);
         }
     }
-    for (; i < lines.length; i++) {
-        back.appendChild(document.createTextNode(lines[i]));
-        back.appendChild(document.createElement("br"));
+
+    function removeBook(title)
+    {
+        let index = findBook(title);
+        if (index == -1)
+        {
+            return `No book named "${title} found.`;
+        }
+        else
+        {
+            books.splice(index, 1);
+            Interface.removeBookElement(title);
+        }
     }
 
-    bookContainer.prepend(book);
-}
+    function findBook(title)
+    {
+        return books.findIndex((book)=>book.title == title);
+    }
 
-function removeBookElement(title)
-{
-    let book = document.querySelector(`.book[bookTitle="${title}"]`);
-    bookContainer.removeChild(book);
-}
+    return {
+        addBook,
+        removeBook
+    }
+})();
 
-// Register books
 
-addBook(
+const Interface = (function(){
+    let bookContainer = document.querySelector("main");
+
+    function addBookElement(title, summary)
+    {
+        let book = document.createElement("div");
+        book.classList.add("book");
+        book.setAttribute("bookTitle", title);
+
+        book.innerHTML = `
+        <div class="cover">
+            <h2></h2>
+        </div>
+        <div class="back">
+            <p></p>
+            <div class="book-actions">
+                <button><img src="images/view.svg" alt="View"></button>
+                <button><img src="images/delete.svg" alt="Remove"></button>
+            </div>
+        </div>
+        <div class="spine"></div>
+        `
+
+        book.querySelector(".cover > h2").textContent = title;
+
+        book.querySelector("button:has(img[alt='Remove'])").addEventListener("click", ()=>{
+            removeBook(title);
+        });
+
+        let back = book.querySelector(".back > p");
+        let lines = summary.split("\n");
+        let i = 0;
+        for (; i < lines.length; i++)
+        {
+            if (lines[i].trim() != '')
+            {
+                break;
+            }
+        }
+        for (; i < lines.length; i++) {
+            back.appendChild(document.createTextNode(lines[i]));
+            back.appendChild(document.createElement("br"));
+        }
+
+        bookContainer.prepend(book);
+    }
+
+    function removeBookElement(title)
+    {
+        let book = document.querySelector(`.book[bookTitle="${title}"]`);
+        bookContainer.removeChild(book);
+    }
+
+    return {
+        addBookElement,
+        removeBookElement
+    };
+})();
+
+
+const Bindings = (function(){
+    let addBookForm = document.querySelector("#add-book-form");
+    let bookTitleField = addBookForm.querySelector("#title");
+    let bookSummaryField = addBookForm.querySelector("#summary");
+    let bookErrorField = addBookForm.querySelector("#error");
+
+    let addBookButton = document.querySelector("#add-book");
+    let cancelAddBookButton = document.querySelector("#cancel-add-book");
+
+    addBookButton.addEventListener("click", (event)=>{
+        bookTitleField.value = "";
+        bookSummaryField.value = "";
+        bookErrorField.textContent = "";
+
+        addBookForm.classList.remove("hidden");
+    });
+
+    cancelAddBookButton.addEventListener("click", (event)=>{
+        addBookForm.classList.add("hidden");
+    });
+
+    addBookForm.addEventListener("submit", (event)=>{
+        event.preventDefault();
+
+        let output = addBook(
+            bookTitleField.value, 
+            bookSummaryField.value
+        );
+
+        if (output === undefined)
+        {
+            addBookForm.classList.add("hidden");
+        }
+        else
+        {
+            bookErrorField.textContent = output;
+        }
+    })
+})();
+
+
+Database.addBook(
     "A sea apart", 
     `
     Anna was shocked when her twin sister, Lucia, who she thought was always inseparable with, suddenly left her to study abroad.
@@ -107,7 +158,7 @@ addBook(
     `
 );
 
-addBook(
+Database.addBook(
     "Adventure of a Jhonny", 
     `
     Jhonny's back again! Now with another adventure with you guessed it- Rick and Mike! 
@@ -116,7 +167,7 @@ addBook(
     `
 );
 
-addBook(
+Database.addBook(
     "Anywhere but here",
     `
     "Please... Anywhere but here..." the little boy said to the fairy, he's bored of his home, his life, his bedroom, dark rings are clearly visible under his eyes.
@@ -124,70 +175,30 @@ addBook(
     `
 );
 
-addBook(
+Database.addBook(
     "Morguean'fitness",
     `
     If you aren't fit, you'll end up in the morgue sooner! Any 5 year ol' can tell you that! So pick this book up, the sooner the better, and learn and follow and keep on trainin'!
     `
 );
 
-addBook(
+Database.addBook(
     "Life in ancient Egypt",
     `
     History of the civilization of ancient Egypt, a lively description of the occupation, food, trade, locations of interest, and beliefs of our ancestors along the Nile!
     `
 );
 
-addBook(
+Database.addBook(
     "Grimm's fairytales",
     `
     A complete collection of the original Grim(m's) fairytales, with much violence, vengeance and justice!
     `
 );
 
-addBook(
+Database.addBook(
     "The jester who farts songs",
     `
     There was once, a court jester, who farted songs from his behind. The jester had some real mad skills, and this story has some real weird truths, open this book to find out who, and where this old fart stunk the court!
     `
 );
-
-// Ui bindings
-
-let addBookForm = document.querySelector("#add-book-form");
-let bookTitleField = addBookForm.querySelector("#title");
-let bookSummaryField = addBookForm.querySelector("#summary");
-let bookErrorField = addBookForm.querySelector("#error");
-
-let addBookButton = document.querySelector("#add-book");
-let cancelAddBookButton = document.querySelector("#cancel-add-book");
-
-addBookButton.addEventListener("click", (event)=>{
-    bookTitleField.value = "";
-    bookSummaryField.value = "";
-    bookErrorField.textContent = "";
-
-    addBookForm.classList.remove("hidden");
-});
-
-cancelAddBookButton.addEventListener("click", (event)=>{
-    addBookForm.classList.add("hidden");
-});
-
-addBookForm.addEventListener("submit", (event)=>{
-    event.preventDefault();
-
-    let output = addBook(
-        bookTitleField.value, 
-        bookSummaryField.value
-    );
-
-    if (output === undefined)
-    {
-        addBookForm.classList.add("hidden");
-    }
-    else
-    {
-        bookErrorField.textContent = output;
-    }
-})
